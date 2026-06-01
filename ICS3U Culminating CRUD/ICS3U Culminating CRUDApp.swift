b030e5fbf2f55ebaab028b465bd7a5bff77d8d2e
Ethan Weinbrecht <eweinbrecht@lcs.on.ca>
2026-06-01 14:45:35 -0400

    Initial Commit

---
+++ ICS3U Culminating CRUD/ICS3U Culminating CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3U Culminating CRUDApp.swift
+//  ICS3U Culminating CRUD
+//
+//  Created by Ethan Edmond Weinbrecht on 2026-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ICS3U_Culminating_CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
